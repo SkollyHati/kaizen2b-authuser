61d9914db82eca490468f13d094b07d67045679f
Lumos2020 <lucas.i.gutierrez@gmail.com>
2024-10-23 20:12:47 -0300

    CORS

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const logger        = require('morgan');
 const port = 3005;
 const http = require('http');
 const app = express();
+const cors = require('cors');
+const corsOptions = {
+  credentials: true,
+  origin: ['http://localhost:3000', 'http://localhost:80','http://localhost:5173/'] // Whitelist the domains you want to allow
+};
 
 routerApi(app);
 app.use("/swagger", require("./src/swagger/api-doc"));
 app.use(express.json());
 app.use(logger('dev'));
+app.use(cors(corsOptions));
 /*
 
 */
